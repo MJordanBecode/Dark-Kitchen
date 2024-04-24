09067208aeba033259e169470a9310eeeca69469
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-04-24 10:24:11 +0200

    styled dark mode icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const toggleButton = document.getElementById('darkMode');
 const body = document.body;
+const icon = document.getElementById('iconMode');
 
 toggleButton.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')){
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');  
+  } else{
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
 });
