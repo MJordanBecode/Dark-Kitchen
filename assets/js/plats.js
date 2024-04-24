d767e3fce31500785672fba11cdad1b2f9b99d11
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-04-24 11:21:57 +0200

    added dish description

--- assets/js/plats.js
+++ assets/js/plats.js
@@ -1,31 +1,31 @@
 let menus = [
     {
-        nom: "Burger Chicken",
+        nom: "Chicken Burger",
         image: "https://uiparadox.co.uk/public/templates/royalfare/assets/media/products/p_8.png",
         category: ["Plats", "Poulet"],
         price: "6.99 €",
-        description: "Lorem ipsum dolor sit amet consectetur. Ut in vulputate ac odio.",
+        description: "A crispy chicken burger with a homemade patty, topped with crisp lettuce, fresh tomato, caramelized onions, and a secret dark sauce. Served with crispy fries.",
     },
     {
-        nom: "Poulet tikka masala",
+        nom: "Tikka Masala",
         image: "https://image.noelshack.com/fichiers/2024/17/2/1713905559-pngegg-3.png",
         category: ["Plats", "Poulet", "Salade"],
         price: "11.99 €",
-        description: "Lorem ipsum dolor sit amet consectetur. Ut in vulputate ac odio.",
+        description: "Chicken pieces marinated in a creamy sauce with dark spices and grilled in a tandoor, served with fragrant basmati rice and soft naan bread.",
     },
     {
-        nom: "Pilon de Poulet",
+        nom: "Chicken Drumsticks",
         image: "https://image.noelshack.com/fichiers/2024/17/2/1713905565-pngegg-5.png",
         category: ["Entrée","Soupe", "Poulet"],
-        price: "4.8 €",
-        description: "Lorem ipsum dolor sit amet consectetur. Ut in vulputate ac odio.",
+        price: "6.80 €",
+        description: "Chicken drumsticks marinated in a blend of dark spices, grilled to perfection for deep flavor and tender texture. Served with a dark dipping sauce and crunchy vegetables.",
     },
     {
-        nom: "Poulet de la famille",
+        nom: "Family Chicken Dish",
         image: "https://image.noelshack.com/fichiers/2024/17/2/1713905572-pngegg.png",
         category: ["Plat", "Poulet"],
-        price: "17.90 €",
-        description: "Lorem ipsum dolor sit amet consectetur. Ut in vulputate ac odio.",
+        price: "13.90 €",
+        description: "A generous dish of tender and juicy chicken pieces, slow-cooked with seasonal vegetables in a rich and dark sauce. Served with herbed pilaf rice.",
     },
 ]
 
