a1d409924c487372cedd6d875b6f9af8d363043a
Masy Jordan <masyjordan@gmail.com>
2024-04-24 19:55:58 +0200

    update

---
+++ assets/js/filter.js
@@ -0,0 +1,38 @@
+let menu = [
+    {
+        nom: "Burger Chicken",
+        image: "https://uiparadox.co.uk/public/templates/royalfare/assets/media/products/p_8.png",
+        category: ["Plats", "Poulet"],
+        price: "6.99",
+        description: "p lorem",
+    },
+    {
+        nom: "Salade de poulet",
+        image: "https://image.noelshack.com/fichiers/2024/17/2/1713863344-nourriture-restaurant-removebg-preview.png ",
+        category: ["Plats", "Poulet", "Salade","vegan"],
+        price: "11.99",
+        description: "p lorem",
+    },
+    {
+        nom: "Plateau rustique entre amis",
+        image: "./assets/img/Plateau-rustique-friends.png",
+        category: ["Entrée","Soupe", "Poulet"],
+        price: "4.8",
+        description: "p lorem",
+    },
+    {
+        nom: "Poulet de la famille",
+        image: "https://image.noelshack.com/fichiers/2024/17/2/1713863695-top-view-cooked-spiced-chicken-dark-surface-removebg-preview.png ",
+        category: ["Plat", "Poulet"],
+        price: "17.9",
+        description: "p lorem",
+    },
+]
+
+
+
+const main = document.querySelector('main'); // select the main 
+const mainElement = document.createElement('aside');
+
+main.appendChild(mainElement); // add mainElment in the main
+
