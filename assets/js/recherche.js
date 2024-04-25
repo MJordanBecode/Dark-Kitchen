3371e28f4668c1b4b3a1c8f05f113ccbaed98a5e
Stacy Druart <stacy@StacyDruartMSI>
2024-04-25 16:28:56 +0200

    maj header

--- assets/js/recherche.js
+++ assets/js/recherche.js
@@ -1,91 +1,52 @@
 //------------------------------------------------------js menu------------------------------------
 
 const header = document.createElement("header");
-
 const nav = document.createElement("nav");
 const logo =document.createElement("img");
-const Qualité =document.createElement("a");
+const divmenu=document.createElement("div");
+const divLogo=document.createElement("div")
+const Qualite =document.createElement("a");
 const Contact =document.createElement("a");
 const NosMenu =document.createElement("a");
-
+const modebouton =document.createElement("button");
+document.body.appendChild(modebouton);
 
 logo.setAttribute("src", "./assets/logo2.png");
 logo.classList.add("logo"); //ajouter des classes à élément html en dom
 
-Qualité.setAttribute("href","#");//ajouter des liens hypertext
-// Contact.setAttribute("href","#");//ajouter des liens hypertext
-// NosMenu.setAttribute("href","#");//ajouter des liens hypertext
- 
 
-// debut de a verifier
-// const logoImg = document.createElement("img");
-// const containerDiv = document.createElement("div");
-// const button1=document.createElement("button1");
-// const link1=document.createElement("a");
-// const button2=document.createElement("button2");
-// const link2=document.createElement("a");
-// const button3=document.createElement("button3");
-// const link3=document.createElement("a");
-// const searchInput = document.createElement("input");
+Qualite.setAttribute("href","#");//ajouter des liens hypertext
+Qualite.innerText='Qualité';
 
+Contact.setAttribute("href","#");//ajouter des liens hypertext
+Contact.innerText='Contact';
 
-// logo.className = "logo";
+NosMenu.setAttribute("href","#");//ajouter des liens hypertext
+NosMenu.innerText='Nos menu ';
 
+divLogo.classList.add("Div-logo");
 
-// logoImg.src = "assets/logo2.png";
-// logoImg.alt = "Logo"; 
+divmenu.classList.add("Div-menu");
+modebouton.setAttribute("id","classClic");
 
-
-// containerDiv.className = "container";
-// link1.href = "#";
-// link1.textContent = "Nos menu";
-
-// link2.href="#";
-// link2.textContent="Qualité";
-
-// link3.href="#";
-// link3.textContent="Nous contacter";
-
-
-
-// searchInput.type = "text";
-// searchInput.name = "search";
-// searchInput.placeholder = "Search";
-
-
-// searchInput.addEventListener("keyup", search);
-
-
-
-// // Obtenir le body du document
-// const body = document.getElementsByTagName("body")[0];
-
-// // Insérer le header au début du body
-// body.insertBefore(header, body.firstChild);
-
-// logo.appendChild(logoImg);
-// button1.appendChild(link1);
-// button2.appendChild(link2);
-// button3.appendChild(link3);
-// containerDiv.appendChild(button1);
-// containerDiv.appendChild(button2);
-// containerDiv.appendChild(button3);
-// nav.appendChild(containerDiv);
-// nav.appendChild(logo);
-// nav.appendChild(containerDiv);
-// nav.appendChild(searchInput);
-
-// fin de a verifier
 header.appendChild(nav);
-nav.appendChild(Qualité);
-nav.appendChild(Contact);
-nav.appendChild(NosMenu);
 nav.appendChild(logo);
-nav.appendChild(Qualité);
+nav.appendChild(divLogo);
+divLogo.appendChild(logo);
+nav.appendChild(divmenu);
+divmenu.appendChild(Qualite);
+divmenu.appendChild(Contact);
+divmenu.appendChild(NosMenu);
+nav.appendChild(modebouton);
 
 document.body.appendChild(header);
 
 
+//----------Dark mode-------------------------
+
+  modebouton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+})
 
 // //---------------------------------------------- js barre de recherche----------------------------------------------
 // function search() {
